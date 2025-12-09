b3635444802ed20b4dda18075b3bd64d0baa0bb1
Manish Gowda <gowdamanish200@gmail.com>
2025-12-09 19:35:52 +0530

    new files added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is my new project
 <br>
 This will be my last Project
+<br>
+I am from Bangalore
---
+++ python.py
@@ -0,0 +1,7 @@
+# Simple Python Script
+
+def greet_user():
+    name = input("Enter your name: ")
+    print(f"Hello, {name}! Welcome to Python.")
+
+greet_user()
